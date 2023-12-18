6412ec1f97c22b8b19c3d3bd1f1d4abc346f9e4d
nagasuribabukola <nagasuribabukola@gmail.com>
2023-12-18 19:24:10 +0530

    added s3 backend

---
+++ Multiuser/s3_backend/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = "> 1.0.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.31.0"
+    }
+  }
+  backend "s3" {
+    bucket = "myterraformbucketforbackend"
+    key = "terraformbackends/multiuser"
+    dynamodb_table = "terraformbackend"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
