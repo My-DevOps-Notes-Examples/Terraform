5024e11831963f018b5855f0d508d9b8e498ee3d
nagasuribabukola <nagasuribabukola@gmail.com>
2023-12-19 18:48:11 +0530

    added conditional expression

--- Multiuser/s3_backend/inputs.tf
+++ Multiuser/s3_backend/inputs.tf
@@ -2,3 +2,8 @@ variable "vpc_cidr" {
   type    = string
   default = "192.168.0.0/16"
 }
+
+variable "subnet_dev" {
+  type    = string
+  default = "192.168.0.0/24"
+}
--- Multiuser/s3_backend/network.tf
+++ Multiuser/s3_backend/network.tf
@@ -5,3 +5,13 @@ resource "aws_vpc" "multiuser" {
     Name = format("ntier-%s", terraform.workspace)
   }
 }
+
+resource "aws_subnet" "dev" {
+  count      = terraform.workspace == "dev" ? 1 : 0
+  vpc_id     = aws_vpc.multiuser.id
+  cidr_block = var.subnet_dev
+
+  tags = {
+    Name = format("subnet-%s", terraform.workspace)
+  }
+}
